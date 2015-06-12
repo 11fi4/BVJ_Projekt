3fc826379ad9ee7d6fec42476b1b02379df4b1ee
derSebbl <sebastian.michel96@gmail.com>
2015-06-12 10:18:05 +0200

    Soap Interface Initial Push

---
+++ SoapInterfaceImplementation/SoapInterface/src/com/impl/soapinterface/ResponseBase.java
@@ -0,0 +1,9 @@
+package com.impl.soapinterface;
+
+public class ResponseBase {
+
+	 public int errorCode = 0;
+	 public String errorMessage;
+	 
+	 
+}
---
+++ SoapInterfaceImplementation/SoapInterface/src/com/impl/soapinterface/SoapConnectionManager.java
@@ -0,0 +1,17 @@
+package com.impl.soapinterface;
+
+public class SoapConnectionManager {
+	
+	public ResponseBase return5() {
+		ResponseBase base = new ResponseBase();
+		
+		return base;
+	}
+
+	/*
+	public ResponseBase returnHi(){
+		
+	}
+	*/
+	
+}
