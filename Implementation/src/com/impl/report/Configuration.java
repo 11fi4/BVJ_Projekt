5e8fbdfca46bd6b4387adefa5ed09069e80ecfec
JohannesDrexler <jdrexler94@gmx.de>
2015-06-15 11:39:40 +0200

    *fixed compile error on Configuration

--- Implementation/src/com/impl/report/Configuration.java
+++ Implementation/src/com/impl/report/Configuration.java
@@ -1,48 +1,46 @@
 package com.impl.report;
 
 import java.io.File;
+import java.nio.file.Path;
+
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.*;
 import javax.xml.transform.stream.*;
+
 import org.xml.sax.*;
 import org.w3c.dom.*;
 
 public class Configuration {
-	
+
 	private static Path _tempDir;
-	
-	public static Path GetTempDir
-	{
+
+	public static Path GetTempDir() {
 		return _tempDir;
 	}
-	
+
 	private static boolean _isInitialized;
-	
-	public static boolean IsInitialized()
-	{
+
+	public static boolean IsInitialized() {
 		return _isInitialized;
 	}
-	
-	//TODO initialise
-	public static void Initialise()
-	{
+
+	// TODO initialise
+	public static void Initialise() {
 		File configFile = new File("/cfg/report.cfg.xml");
-		
+
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		
-		//TODO read doc
+		DocumentBuilder builder = null;
+
+		try {
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+
+		// TODO read doc
 		Document doc = null;
-		
-		try
-		{
-			//read doc here
-		}
-		catch()
-		{
-			//TODO catch and log excepiton
-		}
 	}
 
 }
