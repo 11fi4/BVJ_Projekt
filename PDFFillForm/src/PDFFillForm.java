12a7ea0460aa525c9636d5f350cd09a0427f0994
weibeljonas <weibeljonas@gmx.de>
2015-06-16 09:25:23 +0200

    better handling for pdf fill form

--- PDFFillForm/src/PDFFillForm.java
+++ PDFFillForm/src/PDFFillForm.java
@@ -1,5 +1,8 @@
 import java.io.*; 
 import com.justformspdf.pdf.*;
+import java.util.Hashtable;
+import java.util.Enumeration;
+
 
 /** Fills out the form of a given PDF and saves it to a new file */
 public class PDFFillForm {
@@ -22,19 +25,65 @@ public class PDFFillForm {
 	
 	/** Params of the Mahnung form */
 	private String[] Params_Mahnung = new String[]{
-			"eltern_anschrift",
-			"eltern_name",
-			"schueler_name",
-			"schueler_fehltage_sinceDate",
-			"schueler_fehltage_entschuldigt",
-			"schueler_fehltage_unentschuldigt",
-			"schueler_attestpflicht_abDatum",
+			"0",
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10",
+			"11",
+			"12",
+			"13",
+			"14",
+			"15"
 	};
+	
+	
+	Hashtable Generate_Mahnung_Table(){
+		Boolean gender = true;
+		String brief_datum = "05.04.2014";
+		String possesiv0 = gender? "ihre Tochter" : "ihr Sohn";
+		String possesiv1 = gender? "ihrer Tochter" : "ihres Sohnses";
+		String possesiv2 = gender? "unserer Tochter" : "unseres Sohnes";
+		String anschrift = "Gudrun Müller"+"\\rMüllerstr. 7"+"\\r97080 Würzburg";
+		String anrede = (gender? "geehrte Frau " : "geehrter Herr ") + "Müller";
+		String name_schueler = "Tina";
+		String fehltage_seit = "01.01.2014";
+		String fehltage_gesamt = "19";
+		String fehltage_unentschuldigt = "8";
+		String attestpflicht_ab = "17.04.2014";
+		
+		Hashtable<Integer, String> ht = new Hashtable<Integer, String>(); 
+	
+		ht.put(0, anschrift); //0
+		ht.put(1, brief_datum);
+		ht.put(2, anrede + ",");
+		ht.put(3, possesiv1 + " " + name_schueler);
+		ht.put(4, fehltage_seit);
+		ht.put(5, fehltage_gesamt);
+		ht.put(6, fehltage_unentschuldigt);
+		ht.put(7, possesiv1);
+		ht.put(8, possesiv0);
+		ht.put(9, attestpflicht_ab);
+		ht.put(10, possesiv1);
+		ht.put(11, attestpflicht_ab);
+		ht.put(12, brief_datum);
+		ht.put(13, possesiv2 + " " + name_schueler);
+		ht.put(14, fehltage_seit);
+		ht.put(15, attestpflicht_ab);
+		
+		return ht;
+	}
 	
 	/** Testing */
 	public static void main(String[] args){		
-		String INPUT = "T:\\Name.pdf";
-		String OUTPUT = "T:\\Name_filled.pdf";
+		String INPUT = "T:\\Repo_Vorlage_Mahnung.pdf";
+		String OUTPUT = "T:\\Mahnung_filled.pdf";
 		new PDFFillForm().CreateNewPDF(INPUT, OUTPUT);
 	}
 
@@ -42,7 +91,7 @@ public class PDFFillForm {
 	 * Note: Input file must be a PDF 1.4 (set Acrobat DC 5.0 Compatibility)! */	
 	public void CreateNewPDF(String input, String output){
 		try{
-			if(debug) System.out.println("Trying to fill PDF form...");
+			if(debug) System.out.println("Trying to fill PDF form");
 			
 			// Open pdf
 			FileInputStream fis = new FileInputStream(input);
@@ -52,7 +101,7 @@ public class PDFFillForm {
 			Form form = pdf.getForm();
 	
 			// Fill PDF form
-			FillForm(form, FormType.Verweis);
+			FillForm(form, FormType.Mahnung);
 			
 			// Render and Save
 			pdf.render();		
@@ -84,8 +133,15 @@ public class PDFFillForm {
 	private void FillForm(Form form, FormType formType) throws Exception{
 		String[] params = getParams(formType);
 		
-		for(String param: params)
-			FillFormField(form, param, "test");
+		
+		if(debug) System.out.println("Type: " + formType);
+
+		Hashtable ht = Generate_Mahnung_Table();
+		Enumeration e = ht.keys();
+		while(e.hasMoreElements()){
+			int key = (int)e.nextElement();
+			FillFormField(form, ""+key, ht.get(key).toString());
+		}
 	}
 	
 	/** Fills a single field of a form with a value. Skips it field is not found */
