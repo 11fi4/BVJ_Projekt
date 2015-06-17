a13c5dc4767fc11f56de6657c9680c0afd0dd11a
weibeljonas <weibeljonas@gmx.de>
2015-06-17 09:00:59 +0200

    Vorlagen hinzugefügt

--- PDFFillForm/src/PDFFillForm.java
+++ PDFFillForm/src/PDFFillForm.java
@@ -5,14 +5,14 @@ import com.justformspdf.pdf.*;
 import java.util.Hashtable;
 import java.util.Enumeration;
 
-
-/** Fills out the form of a given PDF and saves it to a new file */
+/** Fills out the form of a given PDF and saves it to a new file 
+ * 	IMPORTANT: Input file must be a PDF 1.4 (set Acrobat DC 5.0 Compatibility)! */
 public class PDFFillForm {
-	public enum FormType {Verweis, Nachsitzen, Mahnung};
+	public enum FormType {Verweis, Nachsitzen, Attest};
 	final String PATH = "T:\\";
 	private final boolean debug = true;
 		
-	Hashtable<Integer, String> Generate_Mahnung_Table(){
+	Hashtable<String, String> Generate_Attest_Table(){
 		Boolean gender = true;
 		String brief_datum = "05.04.2014";
 		String possesiv0 = gender? "ihre Tochter" : "ihr Sohn";
@@ -26,24 +26,25 @@ public class PDFFillForm {
 		String fehltage_unentschuldigt = "8";
 		String attestpflicht_ab = "17.04.2014";
 		
-		Hashtable<Integer, String> ht = new Hashtable<Integer, String>(); 
+		Hashtable<String, String> ht = new Hashtable<String, String>(); 
 	
-		ht.put(0, anschrift); //0
-		ht.put(1, brief_datum);
-		ht.put(2, anrede + ",");
-		ht.put(3, possesiv1 + " " + name_schueler);
-		ht.put(4, fehltage_seit);
-		ht.put(5, fehltage_gesamt);
-		ht.put(6, fehltage_unentschuldigt);
-		ht.put(7, possesiv1);
-		ht.put(8, possesiv0);
-		ht.put(9, attestpflicht_ab);
-		ht.put(10, possesiv1);
-		ht.put(11, attestpflicht_ab);
-		ht.put(12, brief_datum);
-		ht.put(13, possesiv2 + " " + name_schueler);
-		ht.put(14, fehltage_seit);
-		ht.put(15, attestpflicht_ab);
+		ht.put("Text1", anschrift); //0
+		ht.put("Text2", brief_datum);
+		ht.put("Text3", anrede + ",");
+		ht.put("Text4", possesiv1 + " " + name_schueler);
+		ht.put("Text5", possesiv1);
+		ht.put("Text6", fehltage_seit);
+		ht.put("Text7", fehltage_gesamt);
+		ht.put("Text8", fehltage_unentschuldigt);
+		ht.put("Text9", possesiv1);
+		ht.put("Text10", possesiv0);
+		ht.put("Text11", attestpflicht_ab);
+		ht.put("Text12", possesiv1);
+		ht.put("Text13", attestpflicht_ab);
+		ht.put("Text14", brief_datum);
+		ht.put("Text15", possesiv2 + " " + name_schueler);
+		ht.put("Text16", fehltage_seit);
+		ht.put("Text17", attestpflicht_ab);
 		
 		return ht;
 	}
@@ -57,30 +58,25 @@ public class PDFFillForm {
 	
 	/** Testing */
 	public static void main(String[] args){		
-		new PDFFillForm().CreateNewPDF(FormType.Mahnung);
+		new PDFFillForm().CreateNewPDF(FormType.Attest);
 	}	
 
-	/** Fills forms of a given PDF and saves it to a new file.
-	 * Note: Input file must be a PDF 1.4 (set Acrobat DC 5.0 Compatibility)! */	
+	/** Fills form of a given PDF formType and saves it to a new file. */	
 	public void CreateNewPDF(FormType formType){
 		try{
 			if(debug) System.out.println("Trying to fill PDF form");
 
-			// Get Paths
 			String INPUT = GetPath(formType, true);
 			String OUTPUT = GetPath(formType, false);
 			
-			// Open PDF
 			FileInputStream fis = new FileInputStream(INPUT);
 			PDFReader reader = new PDFReader(fis);
 			PDF pdf = new PDF(reader);
 			pdf.lk("43B78HB6");
 			Form form = pdf.getForm();
 	
-			// Fill PDF form
 			FillForm(form, formType);
 			
-			// Render and Save
 			pdf.render();		
 			FileOutputStream fos = new FileOutputStream(OUTPUT);
 			pdf.writeTo(fos);
@@ -94,19 +90,19 @@ public class PDFFillForm {
 		}
 	}
 	
-	/** Fills a complete from depending on its FormType */
+	/** Fills a form depending on its FormType */
 	private void FillForm(Form form, FormType formType) throws Exception{
 		if(debug) System.out.println("Type: " + formType);
 
-		Hashtable<Integer, String> ht = Generate_Mahnung_Table();
-		Enumeration<Integer> e = ht.keys();
+		Hashtable<String, String> ht = Generate_Attest_Table();
+		Enumeration<String> e = ht.keys();
 		while(e.hasMoreElements()){
-			int key = (int)e.nextElement();
-			FillFormField(form, ""+key, ht.get(key).toString());
+			String key = (String)e.nextElement();
+			FillFormField(form, key, ht.get(key).toString());
 		}
 	}
 	
-	/** Fills a single field of a form with a value. Skips it field is not found */
+	/** Fills a field of a form with a value. Skips if field is not found */
 	private void FillFormField(Form form, String field, String value){
 		try{
 			((FormText) form.getElement(field)).setValue(value);
@@ -116,7 +112,8 @@ public class PDFFillForm {
 		}	
 	}
 	
-	/** Returns the input (in:true) or output (in:false) path of a formType */
+	/** Returns the input (in:true) or output (in:false) path for a formType 
+	 *  IMPORTANT: Input file must be a PDF 1.4 (set Acrobat DC 5.0 Compatibility)! */
 	private String GetPath(FormType formType, boolean in){
 		switch(formType){
 		case Verweis:
@@ -125,9 +122,9 @@ public class PDFFillForm {
 		case Nachsitzen:
 			if(in) return PATH + "Repo_Vorlage_Nachsitzen.pdf";
 			else return PATH + "Repo_Vorlage_Nachsitzen_filled.pdf";
-		case Mahnung:
-			if(in) return PATH + "Repo_Vorlage_Mahnung.pdf";
-			else return PATH + "Repo_Vorlage_Mahnung_filled.pdf";
+		case Attest:
+			if(in) return PATH + "Repo_Vorlage_Attest.pdf";
+			else return PATH + "Repo_Vorlage_Attest_filled.pdf";
 		} return null;
 	}
 	
