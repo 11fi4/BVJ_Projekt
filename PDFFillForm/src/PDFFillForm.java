777356dfd9572c2fb0a1df85ffb21626a5a8f02f
weibeljonas <weibeljonas@gmx.de>
2015-06-18 11:58:15 +0200

    pdf fill form: verweis generation code added

--- PDFFillForm/src/PDFFillForm.java
+++ PDFFillForm/src/PDFFillForm.java
@@ -1,25 +1,34 @@
-import java.io.*; 
-
-import com.justformspdf.pdf.*;
-
 import java.util.Hashtable;
 import java.util.Enumeration;
+import java.io.*; 
+import com.justformspdf.pdf.*;
 
-/** Fills out the form of a given PDF and saves it to a new file 
- * 	IMPORTANT: Input file must be a PDF 1.4 (set Acrobat DC 5.0 Compatibility)! */
+/** Fills out the form of a given PDF and saves it to a new file */
 public class PDFFillForm {
-	public enum FormType {Verweis, Nachsitzen, Attest};
+	public enum FormType {Verweis, Attest};
 	final String PATH = "T:\\";
 	private final boolean debug = true;
-		
-	Hashtable<String, String> Generate_Attest_Table(){
+
+	/** Generates text table for a FormType */
+	private Hashtable<String, String> Generate_Table(FormType formType){
+		switch(formType){
+		case Verweis:
+			return Generate_Verweis_Table();
+		case Attest:
+			return Generate_Attest_Table();
+		} return null;
+	}
+
+	/** Generates text table for an 'Attest' document */
+	private Hashtable<String, String> Generate_Attest_Table(){
 		Boolean gender = true;
+		Boolean genderParent = true;
 		String brief_datum = "05.04.2014";
 		String possesiv0 = gender? "ihre Tochter" : "ihr Sohn";
 		String possesiv1 = gender? "ihrer Tochter" : "ihres Sohnses";
 		String possesiv2 = gender? "unserer Tochter" : "unseres Sohnes";
 		String anschrift = "Gudrun Müller"+"\\rMüllerstr. 7"+"\\r97080 Würzburg";
-		String anrede = (gender? "geehrte Frau " : "geehrter Herr ") + "Müller";
+		String anrede = (genderParent? "geehrte Frau " : "geehrter Herr ") + "Müller";
 		String name_schueler = "Tina";
 		String fehltage_seit = "01.01.2014";
 		String fehltage_gesamt = "19";
@@ -49,16 +58,30 @@ public class PDFFillForm {
 		return ht;
 	}
 	
-	Hashtable<Integer, String> Generate_Verweis_Table(){ 
-		return new Hashtable();
-	}
-	Hashtable<Integer, String> Generate_Nachsitzen_Table(){ 
-		return new Hashtable(); 
+	/** Generates text table for a 'Verweis' document */ 
+	private Hashtable<String, String> Generate_Verweis_Table(){ 
+		Boolean gender = true;
+		String schuelerBez = gender? "des Schülers" : "der Schülerin";
+		String name_schueler = "Tina Müller";
+		String brief_datum = "05.04.2014";
+		String klasse = "10BJ1";
+		String anschrift = "Gudrun Müller"+"\\rMüllerstr. 7"+"\\r97080 Würzburg";
+		String begruendung = "Unerlaubtes Fernbleiben vom Unterricht";
+		
+		Hashtable<String, String> ht = new Hashtable<String, String>(); 
+	
+		ht.put("Text1", anschrift); //0
+		ht.put("Text2", brief_datum);
+		ht.put("Text3", schuelerBez + " " + name_schueler + ", " + "Klasse " + klasse);
+		ht.put("Text4", begruendung);
+		
+		return ht;
 	}
 	
-	/** Testing */
+	/** For testing purposes */
 	public static void main(String[] args){		
 		new PDFFillForm().CreateNewPDF(FormType.Attest);
+		new PDFFillForm().CreateNewPDF(FormType.Verweis);
 	}	
 
 	/** Fills form of a given PDF formType and saves it to a new file. */	
@@ -74,7 +97,7 @@ public class PDFFillForm {
 			PDF pdf = new PDF(reader);
 			pdf.lk("43B78HB6");
 			Form form = pdf.getForm();
-	
+				
 			FillForm(form, formType);
 			
 			pdf.render();		
@@ -94,7 +117,7 @@ public class PDFFillForm {
 	private void FillForm(Form form, FormType formType) throws Exception{
 		if(debug) System.out.println("Type: " + formType);
 
-		Hashtable<String, String> ht = Generate_Attest_Table();
+		Hashtable<String, String> ht = Generate_Table(formType);
 		Enumeration<String> e = ht.keys();
 		while(e.hasMoreElements()){
 			String key = (String)e.nextElement();
@@ -113,15 +136,12 @@ public class PDFFillForm {
 	}
 	
 	/** Returns the input (in:true) or output (in:false) path for a formType 
-	 *  IMPORTANT: Input file must be a PDF 1.4 (set Acrobat DC 5.0 Compatibility)! */
+	 *  IMPORTANT: Input file must be PDF version 1.4 (set Acrobat DC 5.0 Compatibility)! */
 	private String GetPath(FormType formType, boolean in){
 		switch(formType){
 		case Verweis:
 			if(in) return PATH + "Repo_Vorlage_Verweis.pdf";
 			else return PATH + "Repo_Vorlage_Verweis_filled.pdf";
-		case Nachsitzen:
-			if(in) return PATH + "Repo_Vorlage_Nachsitzen.pdf";
-			else return PATH + "Repo_Vorlage_Nachsitzen_filled.pdf";
 		case Attest:
 			if(in) return PATH + "Repo_Vorlage_Attest.pdf";
 			else return PATH + "Repo_Vorlage_Attest_filled.pdf";
