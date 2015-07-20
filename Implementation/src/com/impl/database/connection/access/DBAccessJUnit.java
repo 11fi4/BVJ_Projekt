377ba78df2c223c329e8b2beddddf5d7ba7ef5c3
n.grande <devlup@outlook.com>
2015-07-20 11:49:35 +0200

    added more unit tests for DBAccessImpl

--- Implementation/src/com/impl/database/connection/access/DBAccessJUnit.java
+++ Implementation/src/com/impl/database/connection/access/DBAccessJUnit.java
@@ -2,7 +2,9 @@ package com.impl.database.connection.access;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -36,5 +38,20 @@ public class DBAccessJUnit {
 		expectedStudent.setBirthdate(new Date(1960, 1, 1));
 		
 		assertEquals("Expected = not Steinam", expectedStudent, accessImpl.getStudentByName("11fi4", "Wallner"));
+	}
+	
+	@Test
+	public void getAllTenStudents() {
+		List<Student> expectedStudentsList = new ArrayList<Student>();
+		Student expectedStudentInList = new Student();
+		expectedStudentInList.setName("Steinam");
+		expectedStudentInList.setAddress("Address");
+		expectedStudentInList.setBirthdate(new Date(1960, 1, 1));
+		
+		for (int i = 0; i < 10; i++) {
+			expectedStudentsList.add(expectedStudentInList);
+		}
+		
+		assertEquals("Expected = 10 Students in list", expectedStudentsList, accessImpl.getAllStudents("11fi4"));
 	}
 }
