added7c3cc1e695e2eb7f892a926784daecdabeb
JohannesDrexler <jdrexler94@gmx.de>
2015-07-22 08:45:25 +0200

    *added json-lib

---
+++ Implementation/src/com/impl/report/JsonHelper.java
@@ -0,0 +1,17 @@
+package com.impl.report;
+
+import org.json.*;
+
+public class JsonHelper {
+
+	public static String ConvertToXml(JSONObject json) {
+		// TODO implement
+		return "";
+	}
+
+	public static boolean Validate(JSONObject json) {
+		// TODO implement
+		return false;
+	}
+
+}
