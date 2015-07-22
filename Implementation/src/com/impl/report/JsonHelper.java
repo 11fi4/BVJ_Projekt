3715f80248890b51a8dac641b149cd9d35359631
JohannesDrexler <jdrexler94@gmx.de>
2015-07-22 08:57:47 +0200

    *implementen JsonValidation (for header)

--- Implementation/src/com/impl/report/JsonHelper.java
+++ Implementation/src/com/impl/report/JsonHelper.java
@@ -1,6 +1,11 @@
 package com.impl.report;
 
+//json
 import org.json.*;
+
+//our enums
+import com.impl.report.DocumentType;
+import com.impl.report.Formats;
 
 public class JsonHelper {
 
@@ -9,9 +14,43 @@ public class JsonHelper {
 		return "";
 	}
 
+	/**
+	 * Validates if the json is valid for further processing
+	 * 
+	 * @param json
+	 *            the JsonObject to use
+	 * @return returns true if validation succeed, else returns false, Excptions
+	 *         will be logged
+	 */
 	public static boolean Validate(JSONObject json) {
-		// TODO implement
-		return false;
-	}
 
+		try {
+
+			// check if header is there
+			JSONObject headerObj = json.getJSONObject("header");
+			if (headerObj == null) {
+				return false;
+			} else {
+				// validate docType
+				String DocType = headerObj.getString("documenttype");
+				if (DocumentType.valueOf(DocType) == null) {
+					return false;
+				}
+
+				// validate Formats
+				String formatValue = headerObj.getString("format");
+				if (Formats.valueOf(formatValue) == null) {
+					return false;
+				}
+			}
+
+			// finally return true if all succeed
+			return true;
+
+		} catch (Exception ex) {
+			// TODO log exception
+			return false;
+		}
+
+	}
 }
