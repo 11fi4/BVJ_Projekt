c35e030d85c5e2577de291aa5ee42e85fb9282dd
n.grande <devlup@outlook.com>
2015-07-24 10:57:32 +0200

    adjusted test cases and fillDB() method

--- Implementation/src/com/impl/database/connection/access/DBAccessImpl.java
+++ Implementation/src/com/impl/database/connection/access/DBAccessImpl.java
@@ -1,5 +1,6 @@
 package com.impl.database.connection.access;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -9,8 +10,13 @@ import org.hibernate.criterion.Restrictions;
 
 import com.impl.database.connection.DBConnectionManager;
 import com.impl.database.connection.DBConnectionManagerImpl;
+import com.impl.database.elements.Absent;
+import com.impl.database.elements.Comment;
+import com.impl.database.elements.Detension;
+import com.impl.database.elements.Parent;
 import com.impl.database.elements.Student;
-import com.impl.database.elements.User;
+import com.impl.database.elements.UserAccount;
+import com.impl.database.elements.Warning;
 
 public class DBAccessImpl implements DBAccess {
 	public DBAccessImpl() {
@@ -22,23 +28,23 @@ public class DBAccessImpl implements DBAccess {
 	private Session session;
 
 	@Override
-	public List<Student> getAllStudents(String className) {
+	public List<Student> getAllStudentsInClass(String className) {
 		// first test implementation - not final
 		Transaction tx = session.beginTransaction();
 
 		Criteria criteria = session.createCriteria(Student.class).add(
-				Restrictions.eq("name", "Steinam_33"));
+				Restrictions.eq("name", className));
 
 		criteria.setMaxResults(10);
 		List<Student> list = criteria.list();
-		Student studentGet = (Student) list.iterator().next();
-		return null;
+		// Student studentGet = (Student) list.iterator().next();
+		return list;
 	}
 
 	@Override
 	public Student getStudentByName(String className, String studentName) {
-		// first test implementation - not final 
-		
+		// first test implementation - not final
+
 		Transaction tx = session.beginTransaction();
 
 		Criteria criteria = session.createCriteria(Student.class)
@@ -47,30 +53,102 @@ public class DBAccessImpl implements DBAccess {
 
 		criteria.setMaxResults(1);
 		Student student = (Student) criteria.list().get(0);
-		
+
 		return student;
 	}
-	
-//	public List<Student> getClassByName(String className) {
-//		Transaction tx = session.beginTransaction();
-//		
-//		Criteria criteria = session.createCriteria(Student.class).add(Restriction.eq("name", className));
-//	}
+
+	// public List<Student> getClassByName(String className) {
+	// Transaction tx = session.beginTransaction();
+	//
+	// Criteria criteria =
+	// session.createCriteria(Student.class).add(Restriction.eq("name",
+	// className));
+	// }
 
 	@Override
-	public User getUser(String username, String password) {
+	public UserAccount getUser(String username, String password) {
 		Transaction tx = session.beginTransaction();
-		
-		Criteria criteria = session.createCriteria(User.class)
+
+		Criteria criteria = session.createCriteria(UserAccount.class)
 				.add(Restrictions.eq("Username", username))
 				.add(Restrictions.eq("Password", password));
-		
-		User user = null;
+
+		UserAccount user = null;
 		if (criteria.list().size() > 0) {
-			user = (User) criteria.list().get(0);
+			user = (UserAccount) criteria.list().get(0);
 		}
-		
+
 		return user;
 	}
-	
+
+	@Override
+	public List<Absent> getStudentAbsents(String studentId) {
+		Transaction tx = session.beginTransaction();
+
+		Criteria criteria = session.createCriteria(Absent.class).add(
+				Restrictions.eq("student_id", studentId));
+
+		List<Absent> list = new ArrayList<Absent>();
+		criteria.list().forEach((instance) -> {
+			list.add((Absent) instance);
+		});
+		return list;
+	}
+
+	@Override
+	public List<Detension> getStudentDetensions(String studentId) {
+		Transaction tx = session.beginTransaction();
+
+		Criteria criteria = session.createCriteria(Detension.class).add(
+				Restrictions.eq("student_id", studentId));
+
+		List<Detension> list = new ArrayList<Detension>();
+		criteria.list().forEach((instance) -> {
+			list.add((Detension) instance);
+		});
+		return list;
+	}
+
+	@Override
+	public List<Comment> getStudentComments(String studentId) {
+		Transaction tx = session.beginTransaction();
+
+		Criteria criteria = session.createCriteria(Comment.class).add(
+				Restrictions.eq("student_id", studentId));
+
+		List<Comment> list = new ArrayList<Comment>();
+		criteria.list().forEach((instance) -> {
+			list.add((Comment) instance);
+		});
+		return list;
+	}
+
+	@Override
+	public List<Parent> getStudentParents(String studentId) {
+		Transaction tx = session.beginTransaction();
+
+		Criteria criteria = session.createCriteria(Parent.class).add(
+				Restrictions.eq("student_id", studentId));
+
+		List<Parent> list = new ArrayList<Parent>();
+		criteria.list().forEach((instance) -> {
+			list.add((Parent) instance);
+		});
+		return list;
+	}
+
+	@Override
+	public List<Warning> getStudentWarnings(String studentId) {
+		Transaction tx = session.beginTransaction();
+
+		Criteria criteria = session.createCriteria(Warning.class).add(
+				Restrictions.eq("student_id", studentId));
+
+		List<Warning> list = new ArrayList<Warning>();
+		criteria.list().forEach((instance) -> {
+			list.add((Warning) instance);
+		});
+		return list;
+	}
+
 }
