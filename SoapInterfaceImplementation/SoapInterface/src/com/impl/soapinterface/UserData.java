b9cda44ee01754ea6eb28d68a3aeae7f980d5244
Sebastian Michel <Administrator@A111-81.kbbz.ads>
2015-07-28 09:28:21 +0200

    Configuration changes, db implementation

--- SoapInterfaceImplementation/SoapInterface/src/com/impl/soapinterface/UserData.java
+++ SoapInterfaceImplementation/SoapInterface/src/com/impl/soapinterface/UserData.java
@@ -1,11 +1,17 @@
 package com.impl.soapinterface;
 
-import com.impl.database.connection.access.DBAccessImpl;;
+import com.impl.database.connection.access.DBAccessImpl;
+import com.impl.database.elements.UserAccount;;
 
 public class UserData {
 
 	public static void addUser(String firstName, String lastName, Integer permissionId, String username, String password){
-		
+		SoapConnectionManager.dbAccess.insertUser(firstName, lastName, username, password);
 	}
 	
+	public static int getUserId(String username, String password){
+		UserAccount account = SoapConnectionManager.dbAccess.requestUser(username, password);
+		
+		return account.getUserAccount_id();
+	}
 }
