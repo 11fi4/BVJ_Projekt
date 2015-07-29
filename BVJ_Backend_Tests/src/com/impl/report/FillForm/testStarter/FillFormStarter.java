fd4be54082f98b03c56248475279e0f1eabc0d09
unknown <j.drexler@A108-09.kbbz.ads>
2015-07-29 09:13:55 +0200

    Seperated Tests from BackEnd and moved to testProject

---
+++ BVJ_Backend_Tests/src/com/impl/report/FillForm/testStarter/FillFormStarter.java
@@ -0,0 +1,18 @@
+package com.impl.report.FillForm.testStarter;
+
+import com.impl.report.DocumentType;
+import com.impl.report.FillForm.DocxFillForm;
+
+public class FillFormStarter {
+
+	/**
+	 * For testing purposes
+	 * 
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		new DocxFillForm(true, "T:\\").CreateNewDocx(DocumentType.Attest);
+		new DocxFillForm(true, "T:\\").CreateNewDocx(DocumentType.Verweis);
+	}
+
+}
--- Implementation/src/com/impl/report/FillForm/testStarter/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Dient nur zum starten des FillForm projektes
- * 
- * @author JonasWeibel
- *
- */
-package com.impl.report.FillForm.testStarter;
